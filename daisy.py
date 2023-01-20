a645245f356874c52680fa8c3293eb7d323d72b6
alex <alex>
2023-01-20 23:12:41 +0100

    added argc req

--- daisy.py
+++ daisy.py
@@ -81,6 +81,9 @@ def dl(magnet, save_path):
 
 
 if '__name__' == '__main__':
+    if sys.argc < 4:
+        print('invalid amount of args')
+        exit(1)
     if sys.argv[1] not in ['movie', 'other', 'show']:
         print('invalid torrent type')
         exit(1)
